4ac749968a65649e3f6627af01361696b4b8e251
IgorrVvorobyov <95999049+IgorrVvorobyov@users.noreply.github.com>
2022-04-06 17:08:21 +0300

    Homework#34

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,103 @@
 import React from 'react';
 import './App.css';
 import Timer from "./components/Timer";
+import {Card, CardActionArea, CardContent, Typography, CardMedia, Grid} from "@mui/material";
 
 function App() {
-  return <Timer/>
+    return (
+        <>
+            <Grid container spacing={4} paddingLeft={20} paddingTop={5}>
+                <Grid item xs={3}>
+                    <Card sx={{maxWidth: 245}}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image="https://i0.wp.com/www.touristisrael.com/wp-content/uploads/2020/01/210910570-4.jpg?fit=805%2C405&ssl=1"
+                                alt="Jerusalem"
+                            />
+                            <CardContent>
+                                <Typography align={"center"} noWrap gutterBottom variant="h5" component="div">
+                                    Jerusalem
+                                </Typography>
+                                <Typography align={"center"} variant="body2" color="text.secondary">
+                                    <Timer city={''} timezone={'Asia/Jerusalem'}/>
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid item xs={3}>
+                    <Card sx={{maxWidth: 245}}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image="https://a.cdn-hotels.com/gdcs/production0/d1645/0c67ff64-cf54-4886-91dd-89aa601463af.jpg?impolicy=fcrop&w=800&h=533&q=medium"
+                                alt="Australia"
+                            />
+                            <CardContent>
+                                <Typography align={"center"} gutterBottom variant="h5" component="div">
+                                    Australia
+                                </Typography>
+                                <Typography align={"center"} variant="body2" color="text.secondary">
+                                    <Timer city={''} timezone={'Australia/Sydney'}/>
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid item xs={3}>
+                    <Card sx={{maxWidth: 245}}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image="https://travelfoot.com/wp-content/uploads/2020/06/how-to-go-from-copenhagen-to-amsterdam.jpg"
+                                alt="Amsterdam"
+                            />
+                            <CardContent>
+                                <Typography align={"center"} gutterBottom variant="h5" component="div">
+                                    Amsterdam
+                                </Typography>
+                                <Typography align={"center"} variant="body2" color="text.secondary">
+                                    <Timer city={''} timezone={'Europe/Amsterdam'}/>
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+                <Grid item xs={3}>
+                    <Card sx={{maxWidth: 245}}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                image="https://blog-www.pods.com/wp-content/uploads/2019/04/MG_1_1_New_York_City-1.jpg"
+                                alt="New_York"
+                            />
+                            <CardContent>
+                                <Typography align={"center"} gutterBottom variant="h5" component="div">
+                                    New-York
+                                </Typography>
+                                <Typography align={"center"} variant="body2" color="text.secondary">
+                                    <Timer city={''} timezone={'America/New_York'}/>
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+            </Grid>
+        </>
+    );
+
+    // <div>
+    //     <Timer zone={'Amsterdam Time'} timezone={'Europe/Amsterdam'}/>
+    //     <Timer zone={'Australia Time'} timezone={'Australia/Sydney'}/>
+    //     <Timer zone={'New-York Time'} timezone={'America/New_York'}/>
+    // </div>
 }
 
 export default App;
+
+
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,12 +2,13 @@ import React, {useEffect} from "react";
 
 type Props = {
     interval?: number;
+    timezone: string;
+    city: string;
 }
 
-const Timer: React.FC<Props> = ({interval}) => {
+const Timer: React.FC<Props> = ({interval, city, timezone}) => {
     const [time, setTime] = React.useState(new Date());
     function tic():void {
-        console.log("kuku");
         setTime(new Date());
     }
 
@@ -15,10 +16,14 @@ const Timer: React.FC<Props> = ({interval}) => {
         setInterval(tic, interval || 1000);
     });
 
-    return <div style={{marginLeft: "50vw"}}>
-        <h3>Current Time</h3>
-        <label>{time.toLocaleTimeString()}</label>
-    </div>
+    return (
+        <>
+        <div>
+            <h3>{city}</h3>
+            <label>{time.toLocaleTimeString('en-GB', {timeZone: timezone})}</label>
+        </div>
+        </>
+    )
 }
 
 export default Timer;
