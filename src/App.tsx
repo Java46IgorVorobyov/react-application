5ab0d93ebaa5016ed708fc8d44bde23984da34fd
IgorrVvorobyov <95999049+IgorrVvorobyov@users.noreply.github.com>
2022-04-12 09:14:22 +0300

    Homework#36/lifeMatrix, position

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
 import React from 'react';
-import './App.css';
-import Timer from "./components/Timer";
-import Color from "./components/Color";
-import timeZones from "./config/time-zones";
-import InputData from "./components/InputData";
-import colors from "./config/colors";
+import lifeGameConfig from "./config/lifeGameConfig.json";
+import Life from "./components/Life";
 
-type ComponentName = 'input' | 'timer' | 'color'| 'Choose of color';
+
 
 function App() {
-    const [timeZone, setTimeZone] = React.useState('Asia/Jerusalem');
-    const [color, setColor] = React.useState('cornflowerblue');
-    const [componentName, setComponentName] = React.useState<ComponentName>('Choose of color');
-
-    const mapComponents: Map<ComponentName, React.ReactNode> = new Map();
-    mapComponents.set('Choose of color', <InputData value={colors} injectValue={setColor}/>);
-    mapComponents.set('color', <Color color={color}/>);
-    mapComponents.set('input', <InputData value={timeZones} injectValue={setTimeZone}/>);
-    mapComponents.set('timer', <Timer timeZone={timeZone}/>);
-
-    return (
-        <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
-            {Array.from(mapComponents.keys()).map(k => <button onClick={() => setComponentName(k)}>{k}</button>)}
-            {mapComponents.get(componentName)}
-        </div>
-    )
+   return (<div>
+      <Life dimension={lifeGameConfig.dimension} ticInterval={lifeGameConfig.tic}/>
+   </div>)
 }
 
 export default App;
