ff2a274770cd337404403cbc0aed7b5e0dc77096
IgorrVvorobyov <95999049+IgorrVvorobyov@users.noreply.github.com>
2022-05-02 18:11:35 +0300

    Homework#39/useImitator

--- src/util/useImitator.ts
+++ src/util/useImitator.ts
@@ -1,4 +1,4 @@
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {ImitatorAction, imitatorActions} from "../config/imitator-config";
 import {addCourse, removeCourse, updateCourse} from "../redux/actions";
 import {getRandomCourse} from "./randomCourse";
@@ -6,12 +6,15 @@ import courseData from "../config/courseData.json"
 import {getRandomNumber} from "./random";
 import {useEffect} from "react";
 import {Course} from "../models/Course";
+import {StateType} from "../redux/store";
+
+let courses: Course[];
 
 export function useImitator() {
+    courses = useSelector<StateType, Course[]>(state => state.courses);
     const dispatch = useDispatch();
     useEffect(() => {
         const intervalId = setInterval(action, 2000);
-
         return () => clearInterval(intervalId)
     }, [])
 
@@ -40,20 +43,25 @@ export function useImitator() {
     }
 
     function dispatchRemove() {
-        //TODO
-        const courses: Course[] = [];
-        const index = getRandomNumber(0, courses.length);
-        const id = courses[index].id;
-        dispatch(removeCourse(id));
+        console.log('removing')
+        if (courses.length != 0) {
+            const index = getRandomNumber(0, courses.length - 1);
+
+            dispatch(removeCourse(courses[index].id));
+            console.log(`removed course with id ${courses[index].id}`)
+        }
+
     }
 
     function dispatchUpdate() {
-        //TODO
-        const courses: Course[] = [];
-        const index = getRandomNumber(0, courses.length);
-        const course = getRandomCourse(courseData);
-        course.id = courses[index].id;
-        dispatch(updateCourse(course));
+        console.log('updating');
+        if (courses.length != 0) {
+            const index = getRandomNumber(0, courses.length - 1);
+            const course = getRandomCourse(courseData);
+            course.id = courses[index].id;
+            dispatch(updateCourse(course));
+            console.log(`updated course with id ${courses[index].id}`)
+        }
     }
 }
 
